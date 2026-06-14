9d4b9eeb0dae570cfd4259bd80122db6498c516d
manpreetkaur80 <manpreet08525@gmail.com>
2026-06-14 16:12:58 +0530

    Fix ThemeContext import case

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './context/Themecontext'
 import App from './App.jsx';
 import './index.css';
 
