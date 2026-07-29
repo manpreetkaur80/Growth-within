575b2efd5cb931fd33f909c1107de7253ebfe51f
manpreet80 <manpreet08525@gmail.com>
2026-07-29 22:07:23 +0530

    main.jsx , understood themecontext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider>
+    <ThemeProvider>                           //calls function from themecontextnew.jsx
       <App />
     </ThemeProvider>
   </StrictMode>
